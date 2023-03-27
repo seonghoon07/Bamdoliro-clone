67fd58184cf71ee342f096603fd20f2dc84bfdf0
seonghoon07 <kkukkuli07@gmail.com>
2023-03-27 17:01:29 +0900

    bamdol clone

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import './App.css';
 import Textbamdol from './components/TextBamdol';
 
 function App() {
-
-
   
   return (
-    <div>
+    <div className='web'>
         <div className='white-bar'>
           <div className='information-bar'>
             <img src={bamdol} className='bamdol'></img>
@@ -19,6 +17,20 @@ function App() {
           </div>
         </div>
         <Textbamdol />
+      
+        <div className='introduce-bamdol'>
+          <div className='draw-bamdol'>
+            <h3 className='team-bamdol'>밤돌이로 팀</h3>
+            <h1 className='real-bamdol'>세상 끝 <br/>'밤돌이로'를 그리다</h1>
+            <div className='longtext-bamdol'>
+              <h2>
+              우리 삶의 실타래는<br/>오직 우리만 풀 수 있어요.<br/>우리는 피하지 않고 담담하게 마주해요.<br/><br/>
+              우리는 동료들과<br/>일상의 행복을 나누며<br/>추억을 쌓아요.<br/><br/>
+              우리를 알리기 위한 가장 탁월한 방법은<br/>사람들이 우리의 창작물 안에서<br/>우리를 찾게 하는거예요.
+              </h2>
+          </div>
+          </div>
+        </div>
     </div>
     
   );
